87bb01dcc0cd12247ffe018073d58f991836c887
nmchaves <17934735+nmchaves@users.noreply.github.com>
2024-05-03 18:49:42 -0700

    Simplify text centering on homepage by moving it to root <main> element (which also means it will be applied to the <h1> now)

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import nicoImg from "@/public/nico_sky_blue_polo.jpeg";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center p-24 max-w-5xl mx-auto">
+    <main className="flex flex-col items-center p-24 max-w-5xl mx-auto text-center">
       <h1 className="text-xl font-semibold mb-12">Hey! I&apos;m Nico Chaves</h1>
       <Image
         src={nicoImg}
@@ -13,17 +13,17 @@ export default function Home() {
         width={200}
         className="rounded-full mb-12"
       />
-      <p className="text-center mb-8">
+      <p className="mb-8">
         I&apos;m a full-stack software engineer with significant experience
         using tools like:
         <br />
         TypeScript, React, Node.js, GraphQL, and SQL (especially PostgreSQL)
       </p>
-      <p className="text-center mb-8">
+      <p className="mb-8">
         When I&apos;m not building software, I like hiking with my dog Sophie,
         doing Muay Thai, spending time in the ocean, and playing bass guitar.
       </p>
-      <p className="text-center">
+      <p>
         Check out my website{" "}
         <Link
           href="https://www.copybass.com/"
