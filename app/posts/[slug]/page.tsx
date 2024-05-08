76b28f915c5d2fdeb5c1055e1ef6fed8d1339522
nmchaves <17934735+nmchaves@users.noreply.github.com>
2024-05-07 22:35:11 -0700

    Horizontally center blog post content

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -18,7 +18,7 @@ export default async function Page({ params }: Props) {
   // to the blog post. This was adapted from the Next.js docs:
   // https://nextjs.org/docs/app/building-your-application/configuring/mdx#using-tailwind-typography-plugin
   return (
-    <div className="prose prose-headings:mt-8 prose-headings:font-semibold prose-h1:text-5xl prose-h2:text-4xl prose-h3:text-3xl prose-h4:text-2xl prose-h5:text-xl prose-h6:text-lg dark:prose-invert">
+    <div className="prose mx-auto prose-headings:mt-8 prose-headings:font-semibold prose-h1:text-5xl prose-h2:text-4xl prose-h3:text-3xl prose-h4:text-2xl prose-h5:text-xl prose-h6:text-lg dark:prose-invert">
       <h1 className="text-center mb-12">{post.meta.title}</h1>
       <div>
         <MDXRemote source={post.content} />
